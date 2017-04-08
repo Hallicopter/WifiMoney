4447ae94964a971c318c5c62874a2f5c849de815
Advait <advait.raykar@gmail.com>
2017-04-08 17:45:25 +0530

    With Dheeraj MB Counter

--- app/src/main/java/com/money/wifi/wifimoney/Sell.java
+++ app/src/main/java/com/money/wifi/wifimoney/Sell.java
@@ -16,50 +16,52 @@ public class Sell extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell);
 
-        setWifiTetheringEnabled(true);
+        HotspotDataWrite();
     }
 
-    public void setWifiTetheringEnabled(boolean enable) {
-        //Log.d(TAG,"setWifiTetheringEnabled: "+enable);
-        String SSID="WoofWoofHotspotWoof"; // my function is to get a predefined SSID
-        String PASS="SpecWoof"; // my function is to get a predefined a Password
+    private void HotspotDataWrite() {
+        WifiManager wifiManager = (WifiManager)getSystemService(WIFI_SERVICE);
 
-        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+        if(wifiManager.isWifiEnabled())
+        {
+            wifiManager.setWifiEnabled(false);
+        }
 
-        if(enable){
-            wifiManager.setWifiEnabled(!enable);    // Disable all existing WiFi Network
-        }else {
-            if(!wifiManager.isWifiEnabled())
-                wifiManager.setWifiEnabled(!enable);
+
+        WifiConfiguration netConfig = new WifiConfiguration();
+
+
+        netConfig.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);
+        netConfig.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
+        netConfig.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
+        netConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+        netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+        netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+        netConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+        netConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+        netConfig.SSID = "EASi";
+        netConfig.preSharedKey = "Sharath";
+
+
+        try{
+
+            Method setWifiApMethod = wifiManager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
+            boolean apstatus=(Boolean) setWifiApMethod.invoke(wifiManager, netConfig,true);
+
+            Method isWifiApEnabledmethod = wifiManager.getClass().getMethod("isWifiApEnabled");
+            while(!(Boolean)isWifiApEnabledmethod.invoke(wifiManager)){};
+
+            Method getWifiApStateMethod = wifiManager.getClass().getMethod("getWifiApState");
+            int apstate=(Integer)getWifiApStateMethod.invoke(wifiManager);
+
+            Method getWifiApConfigurationMethod = wifiManager.getClass().getMethod("getWifiApConfiguration");
+            netConfig=(WifiConfiguration)getWifiApConfigurationMethod.invoke(wifiManager);
+
+            Log.i("Writing HotspotData", "\nSSID:"+netConfig.SSID+"\nPassword:"+netConfig.preSharedKey+"\n");
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        Method[] methods = wifiManager.getClass().getDeclaredMethods();
-        for (Method method : methods) {
-            if (method.getName().equals("setWifiApEnabled")) {
-                WifiConfiguration netConfig = new WifiConfiguration();
-                if(!SSID.isEmpty() || !PASS.isEmpty()){
-                    netConfig.SSID=SSID;
-                    netConfig.preSharedKey = PASS;
-                    netConfig.hiddenSSID = false;
-                    netConfig.status = WifiConfiguration.Status.ENABLED;
-                    netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
-                    netConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
-                    netConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
-                    netConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
-                    netConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
-                    netConfig.allowedProtocols.set(WifiConfiguration.Protocol.RSN);
-                    netConfig.allowedProtocols.set(WifiConfiguration.Protocol.WPA);
-                }
-                try {
-                    method.invoke(wifiManager, netConfig, enable);
-                    //Log.e(TAG,"set hotspot enable method");
-                } catch (Exception ex) {
-                }
-                break;
-            }
-        }
+
     }
-
-
-
-
 }
