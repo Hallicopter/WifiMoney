7b3a294f87bcc20d3fb14a5909bfe12cfe06b7f3
Dheeraj R Reddy <dheeraj98reddy@gmail.com>
2017-04-09 00:06:02 +0530

    Seller done, payment gateway pending

--- app/src/main/java/com/money/wifi/wifimoney/Sell.java
+++ app/src/main/java/com/money/wifi/wifimoney/Sell.java
@@ -5,12 +5,14 @@ import android.content.Intent;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.ActivityCompat;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.Manifest;
 
 import java.lang.reflect.Method;
 import java.util.Random;
@@ -36,22 +38,20 @@ public class Sell extends AppCompatActivity {
     private void getSellerDetails( View view)
     {
         EditText dataInput = (EditText) findViewById(R.id.dataInput);
-        EditText timeInput = (EditText) findViewById(R.id.timeInput);
         EditText rateInput = (EditText) findViewById(R.id.rateInput);
 
-        if(isEmpty(dataInput) || isEmpty(timeInput) || isEmpty(rateInput)) {
+        if(isEmpty(dataInput) || isEmpty(rateInput)) {
             Toast.makeText(this, "Enter all details", Toast.LENGTH_SHORT).show();
             return;
         }
 
         int data = Integer.parseInt(dataInput.getText().toString());
-        int time = Integer.parseInt(timeInput.getText().toString());
         int rate = Integer.parseInt(rateInput.getText().toString());
         String password = randomString(9);
-        Glob sellerGlob = new Glob(password, data, time, rate);
+        Glob sellerGlob = new Glob(password, data, rate);
         String SSID = sellerGlob.getSSID();
         Toast.makeText(this, SSID, Toast.LENGTH_LONG).show();
-        //createHotspot(SSID, password);
+        createHotspot(SSID, password);
     }
 
     private boolean isEmpty(EditText etText ) {
@@ -69,12 +69,26 @@ public class Sell extends AppCompatActivity {
         String output = sb.toString();
         return output;
     }
+    public void createHotspot(String SSID, String password){
+        Context context=getApplicationContext();
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.WRITE_SETTINGS)){
+
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_SETTINGS},
+                    121);
+        }
+        WifiAPController wifiAPController = new WifiAPController();
+        wifiAPController.wifiToggle(SSID, password, wifiManager, context);
+    }
 }
 
 class Glob
 {
     private String password, SSID;
-    private int rate, time, data;
+    private int rate, data, shift;
     public String getSSID() {
         return SSID;
     }
@@ -84,44 +98,36 @@ class Glob
     public int getRate() {
         return rate;
     }
-    public int getTime() {
-        return time;
-    }
     public int getData() {
         return data;
     }
-    public Glob(String password, int data, int time, int rate )
-    {
+    public Glob(String password, int data, int rate ) {
         this.password = password;
-        this.time = time;
         this.data = data;
         this.rate = rate;
         this.SSID = "";
+        this.shift = data - 5 * rate;
         this.encrypt();
     }
 
-    public Glob(String SSID)
-    {
+    public Glob(String SSID) {
         this.SSID = SSID;
         this.password = "";
         this.data = 0;
         this.rate = 0;
-        this.time = 0;
         this.decrypt();
     }
 
-    private void encrypt()
-    {
-        int shift = rate + time + data;
-
+    private void encrypt() {
         for(int i = 0 ; i < password.length(); i++)
             SSID += (char) (password.charAt(i) + shift);
 
         if(SSID.length() % 2 != 0)
             SSID += '.';
 
-        SSID = "AMBMJ" + rate + SSID.substring(0,SSID.length()/2) + time + SSID.substring(SSID.length()/2) + data;
+        SSID = "AMBMJ" + rate + SSID + data;
     }
+
     private void decrypt() {
         SSID = SSID.substring(5);
         for(int i = 0; i < SSID.length(); i++) {
@@ -140,17 +146,7 @@ class Glob
             }
         }
 
-        for(int i = 0; i < SSID.length(); i++) {
-            if(Character.isDigit(SSID.charAt(i))) {
-                int j;
-                for(j = i+1; Character.isDigit(SSID.charAt(j)); j++);
-                time = Integer.parseInt(SSID.substring(i, j));
-                SSID = SSID.substring(0, i) + SSID.substring(j);
-                break;
-            }
-        }
-
-        int shift = time + rate + data;
+        shift = data - 5 * rate;
         int len = SSID.length();
         if(SSID.charAt(len - 1) == '.'){
             len--;
