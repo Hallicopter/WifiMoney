a081530053300a455a7055aec2d400c1d2bf060b
Advait <advait.raykar@gmail.com>
2017-04-09 09:43:16 +0530

    BTC Gateway

---
+++ app/src/main/java/com/money/wifi/wifimoney/Buyer.java
@@ -0,0 +1,13 @@
+package com.money.wifi.wifimoney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Buyer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buyer);
+    }
+}
